02c8071e0ba75a778fb58edc32e41af825150cef
Marcscob <marcscob@hotmail.com>
2024-02-01 20:52:47 -0300

    fgh

--- php-mysql/report.php
+++ php-mysql/report.php
@@ -9,15 +9,21 @@
         // phpinfo();
             $when_it_happened = $_POST['whenithappened'];
             $how_long = $_POST['howlong'];
+            $how_many = $_POST['howmany'];
             $alien_description = $_POST['aliendescription'];
+            $what_they_did = $_POST['whattheydid'];
             $fang_spotted = $_POST['fangspotted'];
+            $other = $_POST['other'];
             $email = $_POST['email'];
 
             echo 'Thanks for submitting the form.<br/>';
             echo 'You were abducted ' . $when_it_happened;
             echo ' and were gone for ' . $how_long . '<br/>';
+            echo 'Number of aliens: ' . $how_many . '<br/>';
             echo 'Describe them: ' . $alien_description . '<br/>';
+            echo 'The aliens did this: ' . $what_they_did . '<br/>';
             echo 'Was Fang there? ' . $fang_spotted . '<br/>';
+            echo 'Other comments: ' . $other . '<br/>';
             echo 'Your email address is: ' .  $email;
 
         ?>
